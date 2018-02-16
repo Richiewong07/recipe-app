20c1681a59c45cf16b6d893b9a462cc5562868fc
Richie Wong <richiewong07@yahoo.com>
2018-02-15 21:04:43 -0600

    updated

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var cons = require('consolidate');
 var dust = require('dustjs-helpers');
-var pg = require('pg');
+// var pg = require('pg');
+
+const { Pool, Client } = require('pg')
+
 
 app = express();
 
 // DB Connect String
-var connect = 'postgres://richiewong:1168334aN*@localhost/recipebookdb';
+// var connect = 'postgres://richiewong:1168334aN*@localhost/recipebookdb';
 
 // Assign Dust Engine To .dust Files
 app.engine('dust', cons.dust);
@@ -28,19 +31,26 @@ app.get('/', function(req, res) {
   // res.render('index');
   // console.log('TEST');
 
-  pg.connect(connect, function(err, client, done) {
-    if(err) {
-      return console.error('error fetching client from pool', err);
-    }
-    client.query('SELECT * FROM recipes', function(err, result) {
-      if(err) {
-        return console.error('error running query', err);
-      }
-      res.render('index', {recipes: results.row});
-      done();
-    });
-  });
+//   pg.connect(connect, function(err, client, done) {
+//     if(err) {
+//       return console.error('error fetching client from pool', err);
+//     }
+//     client.query('SELECT * FROM recipes', function(err, result) {
+//       if(err) {
+//         return console.error('error running query', err);
+//       }
+//       res.render('index', {recipes: results.row});
+//       done();
+//     });
+//   });
+  const pool = new Pool()
+  pool.query('SELECT NOW * FROM recipes', (err, res) => {
+    console.log(err, res)
+    pool.end()
+  })
+
 })
+
 
 // Server
 app.listen(3000, function() {
